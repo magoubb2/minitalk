ca06796198defc6d1a5d9b3b9959985d0bfcc31c
mabaron- <115954562+magoubb2@users.noreply.github.com>
2023-05-07 20:02:04 -0400

    Add files via upload

--- minitalk/server.c
+++ minitalk/server.c
@@ -1,5 +1,25 @@
 #include "minitalk.h"
 
+void handle_signal(int sig)
+{
+    unsigned char r_byte;
+    int b_count;
+
+    r_byte = 0;
+    b_count = 0;
+    if (sig == SIGUSR1)
+    {
+        r_byte |= 1 << b_count;
+    }
+    b_count++;
+    if (b_count == 8)
+    {
+        printf("Received byte: %d\n", r_byte); // changer pour mon printf
+        r_byte = 0;
+        b_count = 0;
+    }
+}
+
 int main(void)
 {
     
