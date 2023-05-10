fd1e05ca169dc7fac9114424ce317cfbfdc37162
mabaron- <115954562+magoubb2@users.noreply.github.com>
2023-05-09 20:00:13 -0400

    Add files via upload

--- server.c
+++ server.c
@@ -8,6 +8,30 @@ void	get_bits(int signum ,char *c)
 		*c >>= 1;
 }
 
+void sig_handler(int signum, siginfo_t *info, void *context)
+{
+	static	int pid;
+	static	int i;
+	static	char c;
+	
+	(void) context;
+	if (pid == 0)
+		pid = info->si_pid; /* sending process */
+	get_bits(pid, &c);
+	if (i++ == 8)
+	{
+		i = 0;
+		if (!c)
+		{
+			kill(pid, SIGUSR1);
+			pid = 0;
+			return (c);
+		}
+		
+	}
+	kill(pid, SIGUSR2);
+}
+
 int main(void)
 {
     ft_printf("%d", getpid());
