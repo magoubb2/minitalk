81083036186ae594b157883fa56c1fefcd644530
mabaron- <115954562+magoubb2@users.noreply.github.com>
2023-05-13 16:40:49 -0400

    Add files via upload

--- client.c
+++ client.c
@@ -1,25 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabaron- <mabaron-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 10:35:34 by mabaron-          #+#    #+#             */
+/*   Updated: 2023/05/13 12:55:38 by mabaron-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <signal.h>
 #include "minitalk.h"
 
-void	send_bits(unsigned char byte, int pid)
+//static int	g_var; /* to know when we stop receiving signal */
+
+
+
+/*void print_byte(char c) {
+    for (int i = 7; i >= 0; i--) {
+        printf("%d", (c >> i) & 1);
+    }
+    printf("\n");
+}
+*/
+
+void	sig_handler(int signum, siginfo_t *info, void *context)
+{
+	static int	i = 0; /* to keep track of the number of bits */
+	
+	(void) info;
+	(void) context;
+	if (signum == SIGUSR2)
+		i++;
+	else if (signum == SIGUSR1)
+		ft_printf("Number of byte received%d\n:", i/8);
+	
+}
+
+void	send_bits(char byte, int pid)
 {
     int i;
+	int bit_idx;
 
-    i = 128;
+    bit_idx = 7;
     
-    while(i >= 0)
+	//print_byte(byte);
+    while(bit_idx >= 0)
     {
-        if (byte & i)
+		i = 0;
+        if ((byte >> bit_idx) & 1)
             kill(pid, SIGUSR1);
         else
             kill(pid, SIGUSR2);
-        i >>= 1;
-        usleep(100);
+		usleep(100);
+		bit_idx--;
     }
 }
 
-void    send_message(char *str, int pid)
+/*void    send_message(char *str, int pid)
 {
     int i;
 
@@ -30,18 +70,28 @@ void    send_message(char *str, int pid)
         i++;
     }
 }
+*/
 
 int main(int argc, char *argv[])
 {
-    int s_pid;
-    char *mess;
+	struct sigaction	sa;
+    int					s_pid;
+	int					byte_idx;
 
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = sig_handler;
+    s_pid = ft_atoi(argv[1]);
+	byte_idx = 0;
     if (argc != 3)
     {
+		ft_printf("%s", "WRONG NUMBER OF ARGUMENTS");
         return (0);
-    }
-    s_pid = ft_atoi(argv[1]);
-    mess = argv[2];
-    send_message(mess, s_pid);
+	}
+	if (sigaction(SIGUSR1, &sa, NULL) == 1)
+		ft_printf("%s" "error_1");
+	if (sigaction(SIGUSR1, &sa, NULL) ==  1)
+		ft_printf("%s" "error_2");
+	while (argv[2][byte_idx])
+		send_bits(argv[2][byte_idx++], s_pid);
     return (0);
 }
--- server.c
+++ server.c
@@ -1,38 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabaron- <mabaron-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 10:35:44 by mabaron-          #+#    #+#             */
+/*   Updated: 2023/05/13 11:53:37 by mabaron-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
+
+/*void print_byte(char c) {
+    for (int i = 7; i >= 0; i--) {
+        printf("%d", (c >> i) & 1);
+    }
+    printf("\n");
+}
+*/
 
 void	get_bits(int signum ,char *c)
 {
-	if (signum = SIGUSR1)
-		*c = (*c >> 1) | 1;
-	else if (signum >> SIGUSR2)
-		*c >>= 1;
+	if (signum == SIGUSR1)
+		*c = (*c << 1) | 1;
+	else if (signum == SIGUSR2)
+		*c <<= 1;
 }
+
 
 void sig_handler(int signum, siginfo_t *info, void *context)
 {
-	static	int pid;
-	static	int i;
-	static	char c;
+	static	int pid = 0;
+	static	int i = 0;
+	static	char c = 0;
 	
 	(void) context;
 	if (pid == 0)
 		pid = info->si_pid; /* sending process */
-	get_bits(pid, &c);
-	if (i++ == 8)
+	get_bits(signum, &c);
+	i++;
+	if (i == 8)
 	{
 		i = 0;
 		if (!c)
 		{
-			kill(pid, SIGUSR1);
+			//kill(pid, SIGUSR1);
 			pid = 0;
-			return (c);
+			return ;
 		}
-		
+		//print_byte(c);
+		write(1, &c, 1);
+		c = 0;
 	}
-	kill(pid, SIGUSR2);
+	//kill(pid, SIGUSR2);
 }
 
 int main(void)
 {
-    ft_printf("%d", getpid());
+	struct sigaction	sa;
+
+    ft_printf("SERVER PID: %d\n", getpid());
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = sig_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
 }
